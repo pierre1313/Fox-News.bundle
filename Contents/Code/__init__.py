bdb6ca5d8283bc1d4246e5082f6fb9dbdac6e575
Elan Feingold <elan@bluemandrill.com>
2010-09-30 17:33:59 -1000

    Fix weird XPath syntax causing an issue under Windows

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -32,7 +32,7 @@ def Start():
 def MainMenu():
   dir = MediaContainer()
 
-  c = HTML.ElementFromURL(BASE_URL, errors='ignore').xpath('/html/body//div[@id="playlist"]/ul/li')
+  c = HTML.ElementFromURL(BASE_URL, errors='ignore').xpath('//div[@id="playlist"]/ul/li')
   for category in c:
     item = category.xpath('./a')
     if len(item) == 1:
@@ -52,7 +52,7 @@ def MainMenu():
 def Category(sender, title):
   dir = MediaContainer(title2=title)
 
-  c = HTML.ElementFromURL(BASE_URL, errors='ignore').xpath('/html/body//div[@id="playlist"]/ul/li/span/a[text()="' + title + '"]/../../ul/li/a')
+  c = HTML.ElementFromURL(BASE_URL, errors='ignore').xpath('//div[@id="playlist"]/ul/li/span/a[text()="' + title + '"]/../../ul/li/a')
   for category in c:
     title = category.text.strip()
     id = re.search('playlist_id=([0-9]+)', category.get('href')).group(1)
